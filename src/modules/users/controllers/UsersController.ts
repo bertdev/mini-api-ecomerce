ef68ec68fd56c3b346a27d36cf4da0a96cbb5e8b
bertdev <herbertmatos10@hotmail.com>
2022-01-15 14:30:49 -0300

    feat: adiciona rotas para users

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -13,7 +13,7 @@ class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email, password } = request.body;
     const createUser = new CreateUserService();
-    const user = createUser.execute({ name, email, password });
+    const user = await createUser.execute({ name, email, password });
 
     return response.status(201).json(user);
   }
