4f1850c5f2640a0b9d0748112efd5311973473a1
bertdev <herbertmatos10@hotmail.com>
2022-01-17 11:44:45 -0300

    feat: adiciona controller de profiles

---
+++ src/modules/users/controllers/ProfileController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import ShowProfileService from '../services/ShowProfileService';
+import UpdateProfileService from '../services/UpdateProfileService';
+
+class ProfileController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+    const showProfileService = new ShowProfileService();
+    const userProfile = await showProfileService.execute({ user_id: id });
+
+    return response.status(200).json(userProfile);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+    const { name, email, password, old_password } = request.body;
+    const updateProfileService = new UpdateProfileService();
+    const userProfileUpdated = await updateProfileService.execute({
+      user_id: id,
+      name,
+      email,
+      password,
+      old_password,
+    });
+
+    return response.status(200).json(userProfileUpdated);
+  }
+}
+
+export default ProfileController;
