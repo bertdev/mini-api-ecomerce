a9efc24751841905a965cfe6e813b2dc4893778f
bertdev <herbertmatos10@hotmail.com>
2022-01-21 11:54:14 -0300

    feat: adiciona service de update de customer

---
+++ src/modules/customers/services/UpdateCustomersService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Customer from '../typeorm/entities/Customer';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+}
+
+class UpdateCustomersService {
+  public async execute({ id, name, email }: IRequest): Promise<Customer> {
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const customer = await customersRepository.findById(id);
+    if (!customer) {
+      throw new AppError('Customer does not found', 404);
+    }
+
+    const emailExists = await customersRepository.findByEmail(email);
+    if (emailExists && emailExists.id !== id) {
+      throw new AppError('This email is already in use');
+    }
+
+    customer.email = email;
+    customer.name = name;
+    await customersRepository.save(customer);
+
+    return customer;
+  }
+}
+
+export default UpdateCustomersService;
