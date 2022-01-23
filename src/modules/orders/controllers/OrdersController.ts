f2e6a3ae5f56f2afd76b47eea36e41fc4cf3a7d3
bertdev <herbertmatos10@hotmail.com>
2022-01-23 19:45:47 -0300

    feat: adicona controller para orders

---
+++ src/modules/orders/controllers/OrdersController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import CreateOrderService from '../services/CreateOrderService';
+import ShowOrderService from '../services/ShowOrderService';
+
+class OrdersController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const showOrderService = new ShowOrderService();
+    const order = await showOrderService.execute({ id });
+    return response.status(200).json(order);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { customer_id, products } = request.body;
+    const createOrderService = new CreateOrderService();
+    const order = createOrderService.execute({ customer_id, products });
+    return response.status(201).json(order);
+  }
+}
+
+export default OrdersController;
